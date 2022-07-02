b447f348ec33448dc0c778ecacafb9f11ae2f333
BuildTools <unconfigured@null.spigotmc.org>
2022-07-02 17:26:58 -0400

    Add project files.

---
+++ mod.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using System.Globalization;
+
+namespace Mod
+{
+    public class Mod : MonoBehaviour
+    {
+
+        public static string ModTag = " <color=red>[PRMods]"; //Whilst not required, modded items should contain 'tags' at the end of their names to prevent errors in which two mods have an item of the same name.
+        public static string NameTag = " - <color=red>Possibly Remember";
+        public static void Main()
+        {
+            ModAPI.Register(
+                new Modification()
+                {
+                    OriginalItem = ModAPI.FindSpawnable("Brick"),
+                    NameOverride = "Red Keycard" + ModTag,
+                    DescriptionOverride = "Like a brick but differently coloured." + NameTag,
+                    CategoryOverride = ModAPI.FindCategory("Misc."),
+                    ThumbnailOverride = ModAPI.LoadSprite("redkeyc.png"), //Doesn't exist yet!
+                    AfterSpawn = (Instance) =>
+                    {
+                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("redkeyc.png"); //Doesn't exist yet!
+                        Instance.GetComponent<Collider2D>().IsTouching();
+                    }
+                }
+            );
+        }
+    }
+}
