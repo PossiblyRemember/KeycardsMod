39a9dc30462bedb0cdbb6dc010437b74289f4896
BuildTools <unconfigured@null.spigotmc.org>
2022-07-03 18:41:26 -0400

    texure and collison fixes, still trying to optimize the mod

--- mod.cs
+++ mod.cs
@@ -8,7 +8,7 @@ namespace Mod
     public class Mod : MonoBehaviour
     {
 
-        public static string ModTag = " <color=red>[PRMods]"; //Whilst not required, modded items should contain 'tags' at the end of their names to prevent errors in which two mods have an item of the same name.
+        public static string ModTag = " <color=red>[PRMods]";
         public static string NameTag = " - <color=red>Possibly Remember";
         public static void Main()
         //public static Vector2(-4, 0);
@@ -16,38 +16,41 @@ namespace Mod
             ModAPI.Register(
                 new Modification()
                 {
-                    OriginalItem = ModAPI.FindSpawnable("Laser Receiver"),
+                    OriginalItem = ModAPI.FindSpawnable("Laser Pointer"),
                     NameOverride = "Red Keycard" + ModTag,
                     DescriptionOverride = "Red Keycard" + NameTag,
                     CategoryOverride = ModAPI.FindCategory("Misc."),
-                    ThumbnailOverride = ModAPI.LoadSprite("sprites/redkeyc.png"), //Doesn't exist yet!
+                    ThumbnailOverride = ModAPI.LoadSprite("sprites/redkeyc.png"),
                     AfterSpawn = (Instance) =>
                     {
-                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("sprites/redkeyc.png"); //Doesn't exist yet!
+                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("sprites/redkeyc.png",4f,true); //Working on getting this texture to work as I want it to
                         UnityEngine.Object.Destroy(Instance.GetComponent<Collider2D>());
                         Instance.AddComponent<PolygonCollider2D>();
                         Instance.GetComponent<PolygonCollider2D>().pathCount = 2;
                         Instance.GetComponent<PolygonCollider2D>().autoTiling = true;
+                        //UnityEngine.Object.Destroy(Instance.GetComponent<LaserReceiverBehaviour>().Light);
+                        Instance.GetComponent<LaserBehaviour>().barrelPosition = new Vector2(0, 0);
                     }
                 }
             );
             ModAPI.Register(
                 new Modification()
                 {
-                    OriginalItem = ModAPI.FindSpawnable("Laser Pointer"),
+                    OriginalItem = ModAPI.FindSpawnable("Laser Reciever"),
                     NameOverride = "Red Reciever" + ModTag,
                     DescriptionOverride = "Put Red Keycard here," + NameTag,
                     CategoryOverride = ModAPI.FindCategory("Misc."),
-                    ThumbnailOverride = ModAPI.LoadSprite("sprites/redkeyr.png"), //Doesn't exist yet!
+                    ThumbnailOverride = ModAPI.LoadSprite("sprites/redkeyr.png")
                     AfterSpawn = (Instance) =>
                     {
-                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("sprites/redkeyr.png"); //Doesn't exist yet!
-                        UnityEngine.Object.Destroy(Instance.GetComponent<Collider2D>());
-                        Instance.GetComponent<LaserBehaviour>().barrelPosition = new Vector2(-4,0);
-                        Instance.AddComponent<PolygonCollider2D>();
-                        Instance.GetComponent<PolygonCollider2D>().pathCount = 2;
-                        Instance.GetComponent<PolygonCollider2D>().autoTiling = true;
-                        UnityEngine.Object.Destroy(Instance.GetComponent<LaserReceiverBehaviour>().Light);
+                        //Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("sprites/redkeyr.png");
+                        //UnityEngine.Object.Destroy(Instance.GetComponent<Collider2D>());
+                        //Instance.GetComponent<LaserBehaviour>().PhysicalBehaviour.spriteRenderer.sprite = ModAPI.LoadSprite("sprites/redkeyr.png");
+                        //Instance.GetComponent<LaserBehaviour>().barrelPosition = new Vector2(-4,0);
+                        //Instance.AddComponent<PolygonCollider2D>();
+                        //Instance.GetComponent<PolygonCollider2D>().pathCount = 2;
+                        //Instance.GetComponent<PolygonCollider2D>().autoTiling = true;
+                        //Instance.FixColliders();
                     }
                 }
             );
